c6c6d2ea28662c22d1a583004ced5f5f509e4c82
folkert89 <folkertburghout@hotmail.com>
2018-03-28 19:30:43 +0200

    setting up a Person model and connecting it with form fields on SignUp

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import t from 'tcomb-form-native';
+import Person, { formOptions } from '../models/Person';
+
 import styles from './SignUp.styles';
 
 export default class SignUp extends Component {
   render() {
+    const Form = t.form.Form
+
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Sign up for ChatApp</Text>
+      
+        <Form
+          ref="form"
+          type={Person}
+          options={formOptions} />
       </View>
     );
   }
