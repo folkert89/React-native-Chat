a861a011bc138222733692c8d544e53b81aea7cc
folkert89 <folkertburghout@hotmail.com>
2018-03-28 19:09:51 +0200

    create SignUp component with styling

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-export default class App extends React.Component {
+import SignUp from './screens/SignUp';
+import styles from './App.styles';
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>First APP!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <SignUp />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
