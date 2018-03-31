6d24c08da89c687d077f706228e58a430f31d61c
folkert89 <folkertburghout@hotmail.com>
2018-03-31 10:47:55 +0200

    submit button form

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableHighlight } from 'react-native';
 
 import t from 'tcomb-form-native';
 import Person, { formOptions } from '../models/Person';
@@ -7,17 +7,37 @@ import Person, { formOptions } from '../models/Person';
 import styles from './SignUp.styles';
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onSubmit() {
+    const { form } = this.refs
+    const newUser = form.getValue()
+    if (!newUser) return
+    console.log(newUser)
+  }
+  
   render() {
     const Form = t.form.Form
 
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Sign up for ChatApp</Text>
-      
+
         <Form
           ref="form"
           type={Person}
           options={formOptions} />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onSubmit}
+          underlayColor='#99d9f4'>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableHighlight>
       </View>
     );
   }
